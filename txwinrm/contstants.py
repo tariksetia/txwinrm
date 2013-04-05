0d2e4cb6b141c40a3e525d383dc15ff9c18db921
Brian Edwards <bedwards@zenoss.com>
2013-04-05 16:32:18 +0000

    working on handling dates, nils, and subclasses

--- txwinrm/contstants.py
+++ txwinrm/contstants.py
@@ -11,11 +11,14 @@ MAX_PERSISTENT_PER_HOST = 2
 CACHED_CONNECTION_TIMEOUT = 240
 CONNECT_TIMEOUT = 5
 
+XML_NS_SOAP_1_2 = 'http://www.w3.org/2003/05/soap-envelope'
 XML_NS_CIM_SCHEMA = "http://schemas.dmtf.org/wbem/wscim/1/common"
 XML_NS_WS_MAN = 'http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd'
 XML_NS_ENUMERATION = 'http://schemas.xmlsoap.org/ws/2004/09/enumeration'
 XML_NS_MSWSMAN = 'http://schemas.microsoft.com/wbem/wsman/1/wsman.xsd'
 
+SOAP_HEADER = 'Header'
+SOAP_BODY = 'Body'
 WSM_XML_FRAGMENT = 'XmlFragment'
 WSENUM_ENUMERATION_CONTEXT = 'EnumerationContext'
 WSENUM_END_OF_SEQUENCE = 'EndOfSequence'
--- txwinrm/response.py
+++ txwinrm/response.py
@@ -7,40 +7,156 @@
 #
 ##############################################################################
 
-"""WinRM response handlers.
-
-Choices for XML parsers of SOAP responses from WinRM services:
-
-    cElementTree.iterparse
-        clearing elements as they are processed
-        synchronous but fast
-
-    ElementTree.XMLParser
-        asynchronous using feed method
-
-    sax.expatreader.ExpatParser
-        asynchronous using feed method
+"""WinRM response handlers. Parses the SOAP XML responses from WinRM service.
 """
 
 import logging
-from collections import deque
 from cStringIO import StringIO
 from datetime import datetime
-
-from xml.etree import cElementTree
-from xml.etree import ElementTree
+from collections import deque
 from xml import sax
-
 from twisted.internet import defer
 from twisted.internet.protocol import Protocol
 from twisted.web._newclient import ResponseFailed
-
 from . import contstants as c
 
+log = logging.getLogger('zen.winrm')
 
-class EnumerationContextTracker(object):
+
+class SaxResponseHandler(object):
 
     def __init__(self):
+        self._parser = sax.make_parser()
+        self._parser.setFeature(sax.handler.feature_namespaces, True)
+
+    @defer.inlineCallbacks
+    def handle_response(self, response, accumulator):
+        text_buffer = TextBufferingContentHandler()
+        handler = ChainingContentHandler([
+            text_buffer,
+            DispatchingContentHandler(EnvelopeHandlerFactory(text_buffer)),
+        ])
+        self._parser.setContentHandler(handler)
+        reader = ParserFeedingProtocol(self._parser)
+        response.deliverBody(reader)
+        yield reader.d
+        defer.returnValue(self._handler.enumeration_context)
+
+
+class TagComparer(object):
+
+    def __init__(self, uri, localname):
+        self.uri = uri.lower()
+        self.localname = localname.lower()
+
+    def matches(self, uri, localname):
+        return self.uri == uri.lower() \
+            and self.localname == localname.lower()
+
+
+def create_tag_comparer(name):
+    uri, localname = name
+    return TagComparer(uri, localname)
+
+
+class ParserFeedingProtocol(Protocol):
+
+    def __init__(self, xml_parser):
+        self._xml_parser = xml_parser
+        self.d = defer.Deferred()
+        self._debug_data = ''
+
+    def dataReceived(self, data):
+        if log.isEnabledFor(logging.DEBUG):
+            self._debug_data += data
+        self._xml_parser.feed(data)
+
+    def connectionLost(self, reason):
+        if log.isEnabledFor(logging.DEBUG):
+            import xml.dom.minidom
+            xml = xml.dom.minidom.parseString(self._debug_data)
+            log.debug(xml.toprettyxml())
+        if isinstance(reason.value, ResponseFailed):
+            log.error("Connection lost: {0}".format(reason.value.reasons[0]))
+        self.d.callback(None)
+
+
+class ChainingContentHandler(sax.handler.ContentHandler):
+
+    def __init__(self, chain):
+        self._chain = chain
+
+    def startElementNS(self, name, qname, attrs):
+        for handler in self._chain:
+            handler.startElementNS(name, qname, attrs)
+
+    def endElementNS(self, name, qname):
+        for handler in self._chain:
+            handler.endElementNS(name, qname)
+
+
+class TextBufferingContentHandler(sax.handler.ContentHandler):
+
+    def __init__(self, subhandler_factory):
+        self._buffer = StringIO()
+        self.text = None
+
+    def endElementNS(self, name, qname):
+        self.text = self._buffer.getvalue()
+        self._buffer.reset()
+        self._buffer.truncate()
+
+    def characters(self, content):
+        self._buffer.write(content)
+
+
+class DispatchingContentHandler(sax.handler.ContentHandler):
+
+    def __init__(self, subhandler_factory):
+        self._subhandler_factory
+        self._subhandler_tag = None
+        self._subhandler = None
+
+    def startElementNS(self, name, qname, attrs):
+        if self._subhandler is not None:
+            self._subhandler.startElementNS(name, qname, attrs)
+        if self._subhandler is None:
+            self._subhandler, tag = self._get_subhandler_for(name)
+            if self._subhandler is not None:
+                self._subhandler_tag = tag
+
+    def endElementNS(self, name, qname):
+        if self._subhandler_tag is not None:
+            uri, localname = name
+            if self._subhandler_tag.matches(uri, localname):
+                self._subhandler_tag = None
+                self._subhandler = None
+        if self._subhandler is not None:
+            self._subhandler.endElementNS(name, qname)
+
+    def _get_subhandler_for(self, name):
+        tag = create_tag_comparer(name)
+        return self._subhandler_factory.get_handler_for(tag), tag
+
+
+class EnvelopeHandlerFactory(object):
+
+    def __init__(self, text_buffer):
+        self._header_handler = HeaderContentHandler(text_buffer)
+        self._items_handler = ItemsContentHandler(text_buffer)
+
+    def get_handler_for(self, tag):
+        if tag.matches(c.XML_NS_SOAP_1_2, c.SOAP_HEADER):
+            return self._header_handler
+        if tag.matches(c.XML_NS_WS_MAN, c.WSENUM_ITEMS) \
+                or tag.matches(c.XML_NS_ENUMERATION, c.WSENUM_ITEMS):
+            return self._items_handler
+
+
+class HeaderContentHandler(sax.handler.ContentHandler):
+
+    def __init__(self, text_buffer):
+        self._text_buffer
         self._enumeration_context = None
         self._end_of_sequence = False
 
@@ -49,212 +165,76 @@ class EnumerationContextTracker(object):
         if not self._end_of_sequence:
             return self._enumeration_context
 
-    def append_element(self, uri, localname, text):
-        if uri == c.XML_NS_ENUMERATION:
-            if localname == c.WSENUM_ENUMERATION_CONTEXT:
-                self._enumeration_context = text
-            elif localname == c.WSENUM_END_OF_SEQUENCE:
-                self._end_of_sequence = True
+    def endElementNS(self, name, qname):
+        tag = create_tag_comparer(name)
+        if tag.matches(c.XML_NS_ENUMERATION, c.WSENUM_ENUMERATION_CONTEXT):
+            self._enumeration_context = self._text_buffer.text
+        if tag.matches(c.XML_NS_ENUMERATION, c.WSENUM_END_OF_SEQUENCE):
+            self._end_of_sequence = True
 
 
-class TagComparer(object):
+class ItemsContentHandler(sax.handler.ContentHandler):
 
-    def __init__(self, uri, localname):
-        self._uri = uri.lower()
-        self._localname = localname.lower()
+    def __init__(self, text_buffer):
+        self._text_buffer = text_buffer
+        self._nil = False
+        self._tag_stack = deque()
+        self._value = None
 
-    def matches(self, uri, localname):
-        return self._uri == uri.lower() \
-            and self._localname == localname.lower()
-
-
-class EtreeEventHandler(object):
-    """Used by ElementTree and cElementTree parsing"""
-
-    def __init__(self, root, accumulator, resource_uri, cim_class):
-        self._root = root
-        self._accumulator = accumulator
-        self._resource_uri = resource_uri
-        self._cim_class = cim_class
-        self._tracker = EnumerationContextTracker()
-        self._in_items = False
-
-    def handle_event(self, event, elem):
-        if '}' in elem.tag:
-            uri, localname = elem.tag.split('}')
+    def startElementNS(self, name, qname, attrs):
+        if len(self._tag_stack) > 1:
+            raise Exception("tag stack too long: {0}".format(self._tag_stack))
+        tag = create_tag_comparer(name)
+        self._tag_stack.append(tag)
+        if not self._tag_stack:
+            # instance
+            pass
+        elif len(self._tag_stack) == 1:
+            # property
+            pass
+        elif len(self._tag_stack) == 2:
+            # date
+            pass
         else:
-            uri = ''
-            localname = elem.tag
-        uri = uri[1:]
-        tag = TagComparer(uri, localname)
-        if tag.matches(c.XML_NS_WS_MAN, c.WSENUM_ITEMS) \
-                or tag.matches(c.XML_NS_ENUMERATION, c.WSENUM_ITEMS):
-            self._in_items = event == 'start'
-        elif not self._in_items:
-            self._tracker.append_element(uri, localname, elem.text)
-        else:
-            if (tag.matches(self._resource_uri, self._cim_class)
-                    or tag.matches(c.XML_NS_WS_MAN, c.WSM_XML_FRAGMENT)):
-                if event == "start":
-                    self._accumulator.new_instance()
-            elif event == "end":
-                self._accumulator.append_element(uri, localname, elem.text)
-        if event == "end":
-            self._root.clear()
+            
 
-    @property
-    def enumeration_context(self):
-        return self._tracker.enumeration_context
+    def endElementNS(self, name, qname):
+        tag = create_tag_comparer(name)
+        popped_tag = self._tag_stack.pop()
+        if not popped_tag.matches(tag.uri, tag.localname):
+            raise Exception("End of {0} when expecting {1}"
+                            .format(tag.localname, popped_tag.localname))
+
+        if not self._tag_stack:
+            # instance
+            pass
+        elif len(self._tag_stack) == 1:
+            # property
+            pass
+        elif len(self._tag_stack) == 2:
+            # date
+            pass
+
+        self._nil = False
 
 
-class AsyncParseReader(Protocol):
-    """Used by ElementTree and expat parsing"""
 
-    def __init__(self, xml_parser, logger, finished_func=None):
-        self._xml_parser = xml_parser
-        self._logger = logger
-        self._finished = finished_func
-        if finished_func is None:
-            self._finished = xml_parser.finished
-        self._debug_data = ''
-
-    def dataReceived(self, data):
-        if self._logger.isEnabledFor(logging.DEBUG):
-            self._debug_data += data
-        self._xml_parser.feed(data)
-
-    def connectionLost(self, reason):
-        if self._logger.isEnabledFor(logging.DEBUG):
-            import xml.dom.minidom
-            xml = xml.dom.minidom.parseString(self._debug_data)
-            self._logger.debug(xml.toprettyxml())
-        if isinstance(reason.value, ResponseFailed):
-            self._logger.error("Connection lost: {0}".format(
-                reason.value.reasons[0]))
-        self._finished()
+if tag.matches(c.XML_NS_CIM_SCHEMA, "datetime"):
+                if '.' in text:
+                    format = "%Y-%m-%dT%H:%M:%S.%fZ"
+                else:
+                    format = "%Y-%m-%dT%H:%M:%SZ"
+                date = datetime.strptime(text, format)
 
 
-# cElementTree parsing --------------------------------------------------------
-
-class StringIOReader(Protocol):
-
-    def __init__(self, logger):
-        self._logger = logger
-        self.deferred_string_io = defer.Deferred()
-        self._string_io = StringIO()
-        self._debug_data = ''
-
-    def dataReceived(self, data):
-        if self._logger.isEnabledFor(logging.DEBUG):
-            self._debug_data += data
-        self._string_io.write(data)
-
-    def connectionLost(self, reason):
-        if self._logger.isEnabledFor(logging.DEBUG):
-            import xml.dom.minidom
-            xml = xml.dom.minidom.parseString(self._debug_data)
-            self._logger.debug(xml.toprettyxml())
-        if isinstance(reason.value, ResponseFailed):
-            self._logger.error("Connection lost: {0}".format(
-                reason.value.reasons[0]))
-        self._string_io.reset()
-        self.deferred_string_io.callback(self._string_io)
 
 
-class cElementTreeResponseHandler(object):
 
-    def __init__(self, logger):
-        self._logger = logger
+class WinrmContentHandler(object):
 
-    @defer.inlineCallbacks
-    def handle_response(self, response, resource_uri, cim_class, accumulator):
-        reader = StringIOReader(self._logger)
-        response.deliverBody(reader)
-        source = yield reader.deferred_string_io
-        context = cElementTree.iterparse(source, events=("start", "end"))
-        context = iter(context)
-        event, root = context.next()
-        handler = EtreeEventHandler(root, accumulator, resource_uri, cim_class)
-        for event, elem in context:
-            handler.handle_event(event, elem)
-        defer.returnValue(handler.enumeration_context)
-
-
-# ElementTree parsing ---------------------------------------------------------
-
-class WsmanElementTreeParser(ElementTree.XMLParser):
-
-    def __init__(self, html=0, target=None, encoding=None):
-        ElementTree.XMLParser.__init__(self, html, target, encoding)
-        self._handle_event_backlog = deque()
-        self._get_event_backlog = deque()
-
-    def get_event(self):
-        if self._handle_event_backlog:
-            return self._handle_event_backlog.popleft()
-        d = defer.Deferred()
-        self._get_event_backlog.append(d)
-        return d
-
-    def finished(self):
-        self._handle_event(None, None)
-
-    def _start_list(self, tag, attrib_in):
-        """callback from ElementTree.XMLParser"""
-        elem = ElementTree.XMLParser._start_list(self, tag, attrib_in)
-        self._handle_event("start", elem)
-
-    def _end(self, tag):
-        """callback from ElementTree.XMLParser"""
-        elem = ElementTree.XMLParser._end(self, tag)
-        self._handle_event("end", elem)
-
-    def _handle_event(self, event, elem):
-        if self._get_event_backlog:
-            d = self._get_event_backlog.popleft()
-            d.callback((event, elem))
-            return
-        d = defer.Deferred()
-        d.callback((event, elem))
-        self._handle_event_backlog.append(d)
-
-
-class ElementTreeResponseHandler(object):
-
-    def __init__(self, logger):
-        self._logger = logger
-
-    @defer.inlineCallbacks
-    def handle_response(self, response, resource_uri, cim_class, accumulator):
-        parser = WsmanElementTreeParser()
-        reader = AsyncParseReader(parser, self._logger)
-        response.deliverBody(reader)
-        start_event, root = yield parser.get_event()
-        handler = EtreeEventHandler(root, accumulator, resource_uri, cim_class)
-        while True:
-            event, elem = yield parser.get_event()
-            if event is None:
-                break
-            handler.handle_event(event, elem)
-        defer.returnValue(handler.enumeration_context)
-
-
-# sax expat parsing -----------------------------------------------------------
-
-class WsmanSaxContentHandler(sax.handler.ContentHandler):
-
-    def __init__(self, accumulator, resource_uri, cim_class):
-        self._accumulator = accumulator
-        self._resource_uri = resource_uri
-        self._cim_class = cim_class
-        self._tracker = EnumerationContextTracker()
-        self._buffer = StringIO()
+    def __init__(self):
         self._in_items = False
         self._property = None
-
-    @property
-    def enumeration_context(self):
-        return self._tracker.enumeration_context
 
     def startElementNS(self, name, qname, attrs):
         uri, localname, tag = self._element('start', name)
@@ -264,20 +244,11 @@ class WsmanSaxContentHandler(sax.handler.ContentHandler):
             self._property = localname
 
     def endElementNS(self, name, qname):
-        text = self._buffer.getvalue()
-        text = None if not text else text
-        self._buffer.reset()
-        self._buffer.truncate()
         uri, localname, tag = self._element('end', name)
         if localname is None:
             return
         if self._in_items:
-            if tag.matches(c.XML_NS_CIM_SCHEMA, "datetime"):
-                if '.' in text:
-                    format = "%Y-%m-%dT%H:%M:%S.%fZ"
-                else:
-                    format = "%Y-%m-%dT%H:%M:%SZ"
-                date = datetime.strptime(text, format)
+            
                 self._accumulator.append_element(uri, self._property, date)
                 self._property = None
             elif self._property is not None:
@@ -285,8 +256,6 @@ class WsmanSaxContentHandler(sax.handler.ContentHandler):
         else:
             self._tracker.append_element(uri, localname, text)
 
-    def characters(self, content):
-        self._buffer.write(content)
 
     def _element(self, event, name):
         uri, localname = name
@@ -303,23 +272,3 @@ class WsmanSaxContentHandler(sax.handler.ContentHandler):
                 self._accumulator.new_instance()
             return None, None, None
         return uri, localname, tag
-
-
-class ExpatResponseHandler(object):
-
-    def __init__(self, logger):
-        self._logger = logger
-
-    def handle_response(self, response, resource_uri, cim_class, accumulator):
-        parser = sax.make_parser()
-        parser.setFeature(sax.handler.feature_namespaces, True)
-        handler = WsmanSaxContentHandler(accumulator, resource_uri, cim_class)
-        parser.setContentHandler(handler)
-        d = defer.Deferred()
-
-        def finished():
-            d.callback(handler.enumeration_context)
-
-        reader = AsyncParseReader(parser, self._logger, finished)
-        response.deliverBody(reader)
-        return d
