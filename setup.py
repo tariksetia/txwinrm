362dec1425ced55fa7c38eff1803f4622423289e
Brian Edwards <bedwards@zenoss.com>
2013-04-15 21:34:04 +0000

    bumping version to 0.5. Added WinRS support.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from distutils.core import setup
 
 setup(name='txwinrm',
-      version='0.4',
+      version='0.5',
       description='Asynchronous Python WinRM client',
       author='Zenoss',
       author_email='bedwards@zenoss.com',
