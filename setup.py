70cff1355d1b9ccf339e7504a6d2068ef26cff39
Brian Edwards <bedwards@zenoss.com>
2013-05-02 14:33:53 +0000

    bumping version to 0.8.1. trying to get pypi to render README.rst as html

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from distutils.core import setup
 
 setup(name='txwinrm',
-      version='0.8',
+      version='0.8.1',
       description='Asynchronous Python WinRM client',
       author='Zenoss',
       author_email='bedwards@zenoss.com',
