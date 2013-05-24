f23c5cb84e9e12eb5afe64cef2eeb9643c0078b3
Brian Edwards <bedwards@zenoss.com>
2013-05-24 19:39:06 +0000

    bump version for Twisted 11.0.0 fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='0.9.5',
+    version='0.9.6',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
