4c11d654004ae73f54a3027f37fb61542fdc58d8
Dave Bouchillon <dbouchillon@zenoss.com>
2015-01-05 14:42:46 -0600

    Post Release: 1.1.9 -> 1.1.10dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='1.1.9',
+    version='1.1.10dev',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
