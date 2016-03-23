da55d505733b4529483ee51f0a72ced55b66d5c4
Dave Bouchillon <dbouchillon@zenoss.com>
2016-03-23 17:27:14 -0500

    Finish release: Version 1.1.21 -> 1.1.22dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='1.1.21',
+    version='1.1.22dev',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
