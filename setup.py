65d0efe810bd6a639750bfc5f879fe38f1cdf055
Dave Bouchillon <dbouchillon@zenoss.com>
2017-02-21 16:49:39 -0600

    Start Hotfix: Version 1.2.4 -> 1.2.5dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='1.2.4',
+    version='1.2.5dev',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
