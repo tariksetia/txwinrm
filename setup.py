0911d09fdb59d2aa558a562f0ab958b4ab7edeba
Joseph Anderson <j053ph4j@gmail.com>
2017-04-17 09:12:32 -0500

    Begin 1.2.18 Release Version 1.2.17 -> 1.2.18

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 setup_kwargs = dict(
     name='txwinrm',
-    version='1.2.17',
+    version='1.2.18',
     description='Asynchronous Python WinRM client',
     long_description=open('README.rst').read(),
     license='See LICENSE file',
